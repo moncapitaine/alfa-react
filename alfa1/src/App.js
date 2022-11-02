9380b43f8924c6d385092dae07d2d5d16fe21a40
Michael Vogt <michael.vogt@progwise.net>
2022-11-02 11:21:31 +0100

    array handling

--- alfa1/src/App.js
+++ alfa1/src/App.js
@@ -1,11 +1,39 @@
 function App() {
 
-  const hello = function() { return 'hallo aus der Funktion'}
+  const hello = function(name) { return `hallo aus der Funktion, ${name}`}
+
+  const hello2 = (name) => {
+    return `hallo aus hello2, ${name}`
+  }
+
+  const myNumbers = [1,3,6,9]
+
+  const myNewNumbers = myNumbers.map((n, i) => n*(i + 1))
+
+  console.log(myNewNumbers)
+
+  const cars = ['Porsche', 'Mercedes', 'Tesla']
+
+  const carHandler = (c, i) => console.log(c,i)
+  cars.map(carHandler)
+
+  const carHandler2 = (car, index) => (
+      <li>
+        <span>{index}</span>
+        <span>{car}</span>
+      </li>
+    )
 
   return (
     <div>
       <h1>Hallo Michael</h1>
-      <p>{hello()}</p>
+      <p>{hello('Michael')}</p>
+      <p>{hello2('Vogt')}</p>
+      <ul>
+      { cars.map(carHandler2)}
+      </ul>
+      <h2>todo: output of myNumers && myNewNumbers in an Ordered List</h2>
+      ....
     </div>
   );
 }
