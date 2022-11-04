730f8d8d8e1b2d088ab35bf152a98eda36047386
Michael Vogt <michael.vogt@progwise.net>
2022-11-04 16:02:52 +0100

    details sprite

--- alfa2/src/components/Pokemons/pokemonDetails.tsx
+++ alfa2/src/components/Pokemons/pokemonDetails.tsx
@@ -1,22 +1,34 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import {Pokemon} from './pokemonList'
 
 export interface PokemonDetailsProps {
   pokemon: Pokemon 
 }
-const load = async (url: string) => {
+
+export interface PokemonData {
+  sprites: Record<string, string | null>
+}
+const loadDetail = async (url: string): Promise<PokemonData> => {
   const response = await fetch(url)
   const data = await response.json()
-  console.log(data)
+  return data
 }
 export const PokemonDetails = (props: PokemonDetailsProps) => {
-  
+  const [spriteUrl, setSpriteUrl] = useState<string | undefined>(undefined)
   useEffect(() => {
-    load(props.pokemon.url)
+    (async () => {
+      const data = await loadDetail(props.pokemon.url)
+      const keys = Object.keys(data.sprites)
+      const notNullKey = keys.find((key) => data.sprites[key] !== null)
+      if (notNullKey !== undefined) {
+        setSpriteUrl(data.sprites[notNullKey] ?? undefined)
+      }
+    })()
   }, [props.pokemon.url])
 
   return (<article>
     <h2>{props.pokemon.name}</h2>
+    <img src={spriteUrl} alt={props.pokemon.name} />
     <p>{props.pokemon.url}</p>
     </article>)
 }
