1bfcfeeb9284d9e8c83ef97a96fc649c8666edaa
Michael Vogt <michael.vogt@progwise.net>
2022-11-04 11:13:41 +0100

    test nix

--- alfa2/src/components/TestForm/testForm.tsx
+++ alfa2/src/components/TestForm/testForm.tsx
@@ -1,8 +1,20 @@
+import { ChangeEventHandler } from "react"
+
+let testText = "nix"
 export const TestForm = () => {
-  return (<form>
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    testText = e.target.value
+  }
+
+  console.log('aussen log', testText)
+
+  return(<><form>
     <h2>TestForm</h2>
     <label>
-      <input />
+      <input onChange={handleChange} defaultValue={testText} />
     </label>
-  </form>)
+  </form>
+  <div>{testText}</div>
+  </>)
 }
