9727215b5692054d05879a6cb4ab36ed15e674da
Michael Vogt <michael.vogt@progwise.net>
2022-11-07 11:02:35 +0100

    tailwind in alfa3

---
+++ alfa3/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="flex flex-col">
+      <h1 className="text-3xl text-center p-6">Ueberschrift 1</h1>
+      <p className='p-6'>Das ist ein seltsamer Text</p>
+    </div>
+  );
+}
+
+export default App;
