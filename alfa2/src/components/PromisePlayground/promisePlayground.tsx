50f51d9578a0fb524aadf14ca315ef5cfe3233d1
Michael Vogt <michael.vogt@progwise.net>
2022-11-07 09:26:42 +0100

    Todos

--- alfa2/src/components/PromisePlayground/promisePlayground.tsx
+++ alfa2/src/components/PromisePlayground/promisePlayground.tsx
@@ -1,7 +1,12 @@
 import { useEffect, useState } from "react"
 
 const getNewMessage = ():string => (Math.random() + 1).toString(36).substring(7);
- 
+
+const getNewMessageAsync = ():Promise<string> => new Promise()
+
+// Todo1: Implement getNewMessageAsync to return new message after 1234ms using setTimeout
+// Todo2: Use this function to fill the PromisePlayground
+
 export const PromisePlayground = () => {
 
   const [messageList, setMessageList] = useState<string[]>([])
