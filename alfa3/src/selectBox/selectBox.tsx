1ea51e52b8afb39c41ec26e70cc900cf0d283567
Michael Vogt <michael.vogt@progwise.net>
2022-11-08 09:50:38 +0100

    styling selectBox

--- alfa3/src/selectBox/selectBox.tsx
+++ alfa3/src/selectBox/selectBox.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Listbox } from '@headlessui/react'
+import { RiArrowDropDownLine } from 'react-icons/ri'
 
 const people = [
   { id: 1, name: 'Durward Reynolds', unavailable: false },
@@ -9,15 +10,22 @@ const people = [
   { id: 5, name: 'Katelyn Rohan', unavailable: false },
 ]
 
+
+
 export const SelectBox = () => {
   const [selectedPerson, setSelectedPerson] = useState(people[0])
 
   return (
+    <div  className="relative">
     <Listbox value={selectedPerson} onChange={setSelectedPerson}>
-      <Listbox.Button>{selectedPerson.name}</Listbox.Button>
-      <Listbox.Options>
+      <Listbox.Button className="w-full pl-3 pr-1 py-1 rounded-full bg-gray-300 flex items-center justify-between">
+        <span>{selectedPerson.name}</span>
+        <RiArrowDropDownLine size="1.7em" />
+      </Listbox.Button>
+      <Listbox.Options className="border-2 absolute p-2 mb-1 right-1 top-[2em] bg-white rounded-md">
         {people.map((person) => (
           <Listbox.Option
+            className="cursor-pointer py-1 px-2 hover:bg-slate-100 hover:rounded-full"
             key={person.id}
             value={person}
             disabled={person.unavailable}
@@ -27,5 +35,6 @@ export const SelectBox = () => {
         ))}
       </Listbox.Options>
     </Listbox>
+    </div>
   )
 }
