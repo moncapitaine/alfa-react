1d37905b2a141e753a82cb1237ebe4eef926cd55
Michael Vogt <michael.vogt@progwise.net>
2022-11-08 10:42:13 +0100

    select box interface

--- alfa3/src/App.tsx
+++ alfa3/src/App.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SelectBox } from './selectBox/selectBox';
 
+interface Person {
+  id: number
+  name: string
+  unavailable: boolean
+}
+
+const people = [
+  { id: 1, name: 'Durward Reynolds', unavailable: false },
+  { id: 2, name: 'Kenton Towne', unavailable: false },
+  { id: 3, name: 'Therese Wunsch', unavailable: false },
+  { id: 4, name: 'Benedict Kessler', unavailable: true },
+  { id: 5, name: 'Katelyn Rohan', unavailable: false },
+]
+
+
 function App() {
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null)
   return (
     <div className="container flex flex-col">
       <div className="self-center border-2 p-3">
         <h2 className="text-2xl p-3">Select Box Example</h2>
-        <SelectBox />
+        <SelectBox
+          value={selectedPerson !== null ? ({id: selectedPerson?.id.toString(), name: selectedPerson?.name, disabled: selectedPerson?.unavailable}) : null}
+          onChange={(item) => setSelectedPerson({id: parseInt(item.id), name: item.name, unavailable: !!item.disabled})}
+          options={people.map((person) => ({id: person.id.toString(), name: person.name, disabled: person.unavailable }))} />
       </div>
       <h1 className="text-3xl text-center p-6">Ueberschrift 1</h1>
       <p className='p-6 border-[6px] border-[#FF0000]'>Das ist ein seltsamer Text</p>
--- alfa3/src/selectBox/selectBox.tsx
+++ alfa3/src/selectBox/selectBox.tsx
@@ -1,36 +1,38 @@
-import { useState } from 'react'
 import { Listbox } from '@headlessui/react'
 import { RiArrowDropDownLine } from 'react-icons/ri'
 
-const people = [
-  { id: 1, name: 'Durward Reynolds', unavailable: false },
-  { id: 2, name: 'Kenton Towne', unavailable: false },
-  { id: 3, name: 'Therese Wunsch', unavailable: false },
-  { id: 4, name: 'Benedict Kessler', unavailable: true },
-  { id: 5, name: 'Katelyn Rohan', unavailable: false },
-]
+export interface SelectBoxItem {
+  id: string
+  name: string
+  disabled?: boolean
+}
+export interface SelectBoxProps {
+  value: SelectBoxItem | null
+  options: SelectBoxItem[]
+  onChange: (item: SelectBoxItem) => void
+}
 
-
-
-export const SelectBox = () => {
-  const [selectedPerson, setSelectedPerson] = useState(people[0])
-
+export const SelectBox:React.FunctionComponent<SelectBoxProps> = ({ value, options, onChange }) => {
+  const handleValueChange = (item: SelectBoxItem) => {
+    console.log('handleValueChange', item)
+    onChange(item)
+  }
   return (
     <div  className="relative">
-    <Listbox value={selectedPerson} onChange={setSelectedPerson}>
+    <Listbox value={value} onChange={handleValueChange}>
       <Listbox.Button className="w-full pl-3 pr-1 py-1 rounded-full bg-gray-300 flex items-center justify-between">
-        <span>{selectedPerson.name}</span>
+        <span>{value?.name ?? 'Please select item...'}</span>
         <RiArrowDropDownLine size="1.7em" />
       </Listbox.Button>
       <Listbox.Options className="border-2 absolute p-2 mb-1 right-1 top-[2em] bg-white rounded-md">
-        {people.map((person) => (
+        {options.map((option) => (
           <Listbox.Option
-            className="cursor-pointer py-1 px-2 hover:bg-slate-100 hover:rounded-full"
-            key={person.id}
-            value={person}
-            disabled={person.unavailable}
+            className={`cursor-pointer py-1 px-2 hover:bg-slate-100 hover:rounded-full ${option.disabled ? 'text-gray-400 hover:cursor-not-allowed' : ''}`}
+            key={option.id}
+            value={option}
+            disabled={option.disabled}
           >
-            {person.name}
+            {option.name}
           </Listbox.Option>
         ))}
       </Listbox.Options>
