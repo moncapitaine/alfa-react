6284a9208bb65f630b28b4d5e41fec60b5b4f00e
Michael Vogt <michael.vogt@progwise.net>
2022-11-08 08:54:24 +0100

    selectbox example

--- alfa3/src/App.tsx
+++ alfa3/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import { SelectBox } from './selectBox/selectBox';
 
 function App() {
   return (
-    <div className="flex flex-col">
+    <div className="container flex flex-col">
+      <div className="self-center border-2 p-3">
+        <h2 className="text-2xl p-3">Select Box Example</h2>
+        <SelectBox />
+      </div>
       <h1 className="text-3xl text-center p-6">Ueberschrift 1</h1>
       <p className='p-6 border-[6px] border-[#FF0000]'>Das ist ein seltsamer Text</p>
     </div>
---
+++ alfa3/src/selectBox/selectBox.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import { Listbox } from '@headlessui/react'
+
+const people = [
+  { id: 1, name: 'Durward Reynolds', unavailable: false },
+  { id: 2, name: 'Kenton Towne', unavailable: false },
+  { id: 3, name: 'Therese Wunsch', unavailable: false },
+  { id: 4, name: 'Benedict Kessler', unavailable: true },
+  { id: 5, name: 'Katelyn Rohan', unavailable: false },
+]
+
+export const SelectBox = () => {
+  const [selectedPerson, setSelectedPerson] = useState(people[0])
+
+  return (
+    <Listbox value={selectedPerson} onChange={setSelectedPerson}>
+      <Listbox.Button>{selectedPerson.name}</Listbox.Button>
+      <Listbox.Options>
+        {people.map((person) => (
+          <Listbox.Option
+            key={person.id}
+            value={person}
+            disabled={person.unavailable}
+          >
+            {person.name}
+          </Listbox.Option>
+        ))}
+      </Listbox.Options>
+    </Listbox>
+  )
+}
