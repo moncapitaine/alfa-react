53dfb23025385101233dd9440ee949821dd9ba71
Michael Vogt <michael.vogt@progwise.net>
2022-11-14 10:55:39 +0100

    todo timezone switch

--- alfa3/src/testComponent/testComponent2.tsx
+++ alfa3/src/testComponent/testComponent2.tsx
@@ -1,13 +1,17 @@
 import { useEffect, useState } from "react"
 
 export const TestComponent2 = () => {
-  const [currentTime, setCurrentTime] = useState('')
+  const [currentTime, setCurrentTime] = useState('...')
   useEffect(() => {
     setInterval(() => {
       const t = new Date()
       setCurrentTime(`${t.getHours()}:${t.getMinutes()}:${t.getSeconds()}`)
     }, 1000)
   }, [])
+
+  useEffect(() => {
+    console.log('2nd useEffect', currentTime)
+  }, [currentTime])
   // todo: change timezone for clock with buttons
   return (<div className="p-4">
     <button className="m-2 p-1  border-2">UTC-1</button>
