5276d6f3415281aeedd1bf55de3ef86ff4b44600
Michael Vogt <michael.vogt@progwise.net>
2022-11-15 10:23:37 +0100

    text input focus

--- alfa4-vite/src/components/testUseRef2.tsx
+++ alfa4-vite/src/components/testUseRef2.tsx
@@ -1,18 +1,23 @@
 import { useRef, useState } from "react"
 
 export const TestUseRef2 = () => {
-  const [, setIrgendwas ] = useState("")
+  const [textValue1, setTextValue1] = useState("")
+  const [textValue2, setTextValue2] = useState("")
   const textElementRef = useRef<HTMLInputElement>(null)
 
   const handleButtonClick = () => {
-    // focus the text input
-    // read value from the text input
     textElementRef.current?.focus()
-    textElementRef.current?.setAttribute("value", "Michael was here")
-    setIrgendwas("das")
+    setTextValue1("Michael was here")
   }
+
+  const handleButton2Click = () => {
+    setTextValue1(textValue2)
+  }
+
   return (<div>
-    <input type="text" ref={textElementRef} />
+    <input type="text" ref={textElementRef} onChange={(e) => setTextValue1(e.target.value)} value={textValue1} />
+    <input type="text" value={textValue2}  onChange={(e) => setTextValue2(e.target.value)}/>
     <button onClick={handleButtonClick}>Focus to the text</button>
+    <button onClick={handleButton2Click}>Transfer text</button>
   </div>)
 }
